8bd13b4b2bfe94826a36b79a63bdd751cd8b9722
ChaseWelsh <fseusb@gmail.com>
2022-09-20 09:44:55 -0600

    ```ruby array[-1] ``` all good
    ```ruby
    array.slice(1,-1)
    ```
    => nil
    :whyy:

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+    out = ""
+    if array.length>1
+        if array.length == 2
+            out = array.join(" and ")
+        else
+            out = array.slice(0,array.length-1).join(", ") + ", and #{array.last}"
+        end
+    else
+        out = array[0]
+    end
+    out
 end
+
+puts oxford_comma("a,b,c".split(","))
+puts oxford_comma("a,b".split(","))
+puts oxford_comma("a")
